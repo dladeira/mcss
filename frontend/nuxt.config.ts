bd21bfd0b45b3c427ad7341c53ad7fafc6101659
dladeira <dladeiratx@gmail.com>
2023-01-03 08:23:07 +0100

    Fixed Nuxt-Socket-IO complaining when restarting server

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
         'nuxt-socket-io'
     ],
     io: {
+        // server: { teardown: false },
         sockets: [
             {
                 name: 'main',
