9459ce022ac5f7b36d715237f87d5d0c1303df17
dladeira <dladeiratx@gmail.com>
2023-01-22 14:47:26 +0100

    Added in-dev placeholder for landing pages

--- frontend/pages/developers.vue
+++ frontend/pages/developers.vue
@@ -1,16 +1,59 @@
 <template>
     <div>
+
         <Head>
-            <Title>Developers</Title>
+            <Title>Pricing</Title>
         </Head>
 
-        developers
+        <h1 class="page-title">For <span class="title-blue">developers</span></h1>
+
+            <h1 class="title"><span class="title-green">API Access</span> is not out <span class="title-blue">yet</span></h1>
+            <h2 class="subtitle">For more information check out our <nuxt-link class="link" to="/roadmap">roadmap</nuxt-link></h2>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.fake {
+.page-title {
+    margin: 5rem 0 0;
+
+    font-size: 3rem;
+    font-weight: 700;
     color: white;
+}
+
+.title {
+    width: fit-content;
+
+    margin: 9rem auto 1rem auto;
+
+    font-size: 3rem;
+    font-weight: 700;
+    color: white;
+}
+
+.subtitle {
+    width: fit-content;
+    margin: 0 auto;
+
+    font-size: 2rem;
+    font-weight: 400;
+    color: white;
+}
+
+.link {
+    font-weight: 700;
+    text-decoration: underline;
+    color: $green;
+}
+
+// =====
+
+.title-blue {
+    color: $blue;
+}
+
+.title-green {
+    color: $green;
 }
 </style>
 
--- frontend/pages/pricing.vue
+++ frontend/pages/pricing.vue
@@ -1,16 +1,59 @@
 <template>
     <div>
+
         <Head>
             <Title>Pricing</Title>
         </Head>
 
-        for free
+        <h1 class="page-title">Choose your <span class="title-green">plan</span></h1>
+
+            <h1 class="title">For <span class="title-green">free</span>, for <span class="title-blue">now</span>...</h1>
+            <h2 class="subtitle">For more information check out our <nuxt-link class="link" to="/roadmap">roadmap</nuxt-link></h2>
     </div>
 </template>
 
 <style lang="scss" scoped>
-.fake {
+.page-title {
+    margin: 5rem 0 0;
+
+    font-size: 3rem;
+    font-weight: 700;
     color: white;
+}
+
+.title {
+    width: fit-content;
+
+    margin: 9rem auto 1rem auto;
+
+    font-size: 3rem;
+    font-weight: 700;
+    color: white;
+}
+
+.subtitle {
+    width: fit-content;
+    margin: 0 auto;
+
+    font-size: 2rem;
+    font-weight: 400;
+    color: white;
+}
+
+.link {
+    font-weight: 700;
+    text-decoration: underline;
+    color: $green;
+}
+
+// =====
+
+.title-blue {
+    color: $blue;
+}
+
+.title-green {
+    color: $green;
 }
 </style>
 
