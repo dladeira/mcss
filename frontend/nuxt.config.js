b7675a40e7bce1e866453655e8299c93568d70c1
dladiera <dladeiratx@gmail.com>
2024-04-20 01:58:17 +0200

    Fixed proxy not working

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,53 +1,47 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        '@/assets/styles/global.scss'
-    ],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: '@import "@/assets/styles/variables.scss";'
-                }
-            }
+  css: ["@/assets/styles/global.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/variables.scss";',
         },
+      },
     },
-    nitro: {
-        compressPublicAssets: true
-    },
-    modules: [
-        'nuxt-proxy',
-        '@nuxt/image-edge',
-        'nuxt-socket-io'
+  },
+  nitro: {
+    compressPublicAssets: true,
+  },
+  modules: ["nuxt-proxy", "@nuxt/image-edge", "nuxt-socket-io"],
+  io: {
+    server: { teardown: false },
+    sockets: [
+      {
+        name: "main",
+        url: process.env.API_SERVER,
+        default: true,
+      },
     ],
-    io: {
-        server: { teardown: false },
-        sockets: [
-            {
-                name: 'main',
-                url: process.env.API_SERVER,
-                default: true
-            }
-        ]
-    },
-    runtimeConfig: {
-        proxy: {
-            options: [{
-                target: process.env.API_SERVER,
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': '/'
-                },
-                pathFilter: [
-                    '/api'
-                ]
-            }]
+  },
+  runtimeConfig: {
+    proxy: {
+      target: process.env.API_SERVER,
+      options: [
+        {
+          target: process.env.API_SERVER,
+          changeOrigin: true,
+          pathRewrite: {
+            "^/api": "/",
+          },
+          pathFilter: ["/api"],
         },
-        public: {
-            origin: process.env.WEB_SERVER
-        }
+      ],
     },
-    app: {
-        layoutTransition: { name: 'blur', mode: 'out-in' }
-    }
-})
+    public: {
+      origin: process.env.WEB_SERVER,
+    },
+  },
+  app: {
+    layoutTransition: { name: "blur", mode: "out-in" },
+  },
+});
